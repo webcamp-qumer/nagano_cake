e31fefbc216bbc6727650351818e1666f7456ee4
EC2 Default User <ec2-user@ip-172-31-37-183.ec2.internal>
2022-06-18 07:27:23 +0000

    controller1

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,19 @@
+class Public::CartItemsController < ApplicationController
+  def index
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def destroy.all
+  end
+  
+  private
+  
+end
