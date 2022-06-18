a1308bccec7ebdb82c5a1da7f02e4467a33cb08d
nayudesu <srkm.149@gmail.com>
2022-06-18 03:40:31 +0000

    [商品画面]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,33 @@
 class Admin::ItemsController < ApplicationController
+
+
+ def new
+  @item = Item.new
+ end 
+ 
+ def create
+  @item = Item.new(item_params) #ストロングパラメータ
+  @item.admin_id = current_admin.id
+  @item.save
+  redirect_to admin_item_path(@item)
+ end 
+ 
+ def index
+ end 
+ 
+ def show
+ end 
+ 
+ def edit
+ end 
+ 
+ def update
+ end 
+
+private
+
+ def item_params
+  params.require(:item).permit(:name, :message, :price_non_tax) #ジャンルと販売ステータスの追加
+ end 
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   
   namespace :admin do
    root :to =>"homes#top"
-   resources :customers, only: [:index, :show, :edit, :update] 
+   resources :customers, only: [:index, :show, :edit, :update]
+   resources :items, except: [:destroy]
+   
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
