71a0ea8b6f71cebbeb8f7f51ab0c7951791556fe
EC2 Default User <ec2-user@ip-172-31-2-208.ec2.internal>
2022-06-18 07:22:51 +0000

    [新規登録、マイページの追加]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,16 @@
 class Public::CustomersController < ApplicationController
   
   def show
-    
+    @customer = Customer.find(params[:id])
   end
   
   def edit
+    @customer = Customer.find(params[:id])
+    if @customer == current_customer
+      render :edit
+    else
+      redirect_to public_customer_path(current_customer.id)
+    end
     
   end
   def unsubscribe
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   
   #作ったコントローラーの場所を記述
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: { #passwordの変更、sign_up機能の削除
     sessions: "admin/sessions" #管理者はログイン機能のみ
   }
+  
+  
+  #顧客側のルート
+  namespace :public do
+    resources :customers
+  end
+  
+   scope module: :public do
+    root to: 'homes#top'
+  end
   #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
