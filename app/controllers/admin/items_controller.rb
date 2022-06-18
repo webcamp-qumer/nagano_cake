7c43e8ab8ca8f14d5f5f06ab0a6c5ff769f4b20e
nayudesu <srkm.149@gmail.com>
2022-06-18 07:38:35 +0000

    [update]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,18 +7,24 @@ class Admin::ItemsController < ApplicationController
  
  def create
   @item = Item.new(item_params) #ストロングパラメータ
-  @item.admin_id = current_admin.id
-  @item.save
-  redirect_to admin_item_path(@item)
+  @item.admin_id = current_user.id #current_user=ログイン中のユーザーの情報を取得できる
+  if @item.save
+   redirect_to admin_item_path(@item)
+  else
+   render :new
+  end 
  end 
  
  def index
+  @items = Item.all
  end 
  
  def show
+  @item = Item.find(params[:id])
  end 
  
  def edit
+  @item = Item.find(params[:id])
  end 
  
  def update
