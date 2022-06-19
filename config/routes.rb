0020cc09088d8e01b9838172abe7b467f1eb5449
EC2 Default User <ec2-user@ip-172-31-2-208.ec2.internal>
2022-06-19 14:45:28 +0000

    [ルーティングの反映]

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
+
   devise_for :customers, skip: [:passwords],controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  get 'items' => 'public/items#index'
-  get 'items' => 'public/items#show'
-  get 'orders' => 'public/orders#confirm'
-  get 'orders' => 'public/orders#index'
-  get 'orders' => 'public/orders#new'
-  get 'orders' => 'public/orders#show'
-  get 'orders' => 'public/orders#thanks'
-  get 'cart_items' => 'public/cart_items#index'
+#顧客側のルート
+  namespace :public do
+    resources :customers
+  end
+   scope module: :public do
+    root to: 'homes#top'
+    resources :items, only: [:index,:show]
+    resources :orders, only: [:conform,:index,:new,:show,:thanks]
+    resources :cart_items, only: [:index]
+    end
 
   #作ったコントローラーの場所を記述
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: { #passwordの変更、sign_up機能の削除
     sessions: "admin/sessions" #管理者はログイン機能のみ
   }
   namespace :admin do
-    
    root :to =>"homes#top"
-
    resources :customers, only: [:index, :show, :edit, :update]
    resources :items, except: [:destroy]
    resources :genres, only: [:index, :create, :edit, :update]
