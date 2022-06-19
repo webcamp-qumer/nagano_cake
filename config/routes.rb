b479472c30fe798b9b072df1f5ba198a12d4bd76
EC2 Default User <ec2-user@ip-172-31-37-183.ec2.internal>
2022-06-19 07:36:37 +0000

    hozon

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,22 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  
+
   #作ったコントローラーの場所を記述
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: { #passwordの変更、sign_up機能の削除
     sessions: "admin/sessions" #管理者はログイン機能のみ
   }
-  
-  
+
+
   #顧客側のルート
   namespace :public do
     resources :customers
   end
-    
    scope module: :public do
     root to: 'homes#top'
     resources :items, only: [:index,:show]
     resources :orders, only: [:conform,:index,:new,:show,:thanks]
     resources :cart_items, only: [:index]
+    end
   end
- 
-end
+
