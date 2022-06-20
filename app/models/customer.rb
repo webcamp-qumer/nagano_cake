e70fdecc33c4a1c4f039434709987c228474764d
EC2 Default User <ec2-user@ip-172-31-2-208.ec2.internal>
2022-06-20 07:57:07 +0000

    [退会、配送先]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :addresses, dependent: :destroy
+         
   def active_for_authentication?
     super && (is_deleted == false)
   end
