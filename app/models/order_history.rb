4f3bdcb7cd954ea8a8d28feb731f08e2152bbcb1
EC2 Default User <ec2-user@ip-172-31-2-208.ec2.internal>
2022-06-21 11:34:15 +0000

    [TOPの新着一覧]

--- app/models/order_history.rb
+++ app/models/order_history.rb
@@ -3,5 +3,11 @@ class OrderHistory < ApplicationRecord
   belongs_to :order
   
   #空だと保存することができない
-  
+  validates :name, presence: true
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :price_non_tax, presence: true
+  validates :total_price, presence: true
+  validates :amount, presence: true
+  validates :status, presence: true
 end
