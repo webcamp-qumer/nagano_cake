76d09e2f9b1e860ef95517a0bd41ed45d1fac7ca
EC2 Default User <ec2-user@ip-172-31-2-208.ec2.internal>
2022-06-27 06:15:47 +0000

    [last]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,5 +6,19 @@ class Admin::OrdersController < ApplicationController
     @total = 0 #最初に０を代入
   end
   
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(order)
+    else
+      render 'show'
+    end
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
   
 end
